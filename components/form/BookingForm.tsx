d78e7093c14ba34d28142eeffda3fac65b6a17ab
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-10-26 00:54:11 +0530

    ssr

---
+++ components/form/BookingForm.tsx
@@ -0,0 +1,276 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
+import { convertToOrdinal } from "@/lib/utils";
+import { Plus, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast } from "sonner";
+import { createMakeup } from "@/lib/actions/makeup.action";
+
+const BookingForm = () => {
+  const schema = z.object({
+    name: z.string().min(3).max(30),
+    contact: z.string().min(10).max(10),
+    occasion: z.string(),
+    skinAllergy: z.string(),
+    alternateContact: z.string(),
+    makeups: z
+      .array(
+        z.object({
+          location: z.string().min(3).max(100),
+          date: z.date().min(new Date()),
+          readyTime: z.string().min(1).max(30),
+        })
+      )
+      .min(1, "Please add at least one makeup slot"),
+  });
+
+  type Schema = z.infer<typeof schema>;
+  const form = useForm<Schema>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      contact: "",
+      occasion: "",
+      skinAllergy: "",
+      alternateContact: "",
+      makeups: [
+        {
+          location: "",
+          date: new Date(),
+          readyTime: "",
+        },
+      ],
+    },
+    mode: "all",
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "makeups",
+  });
+
+  const onSubmit = (data: Schema) => {
+    return new Promise((resolve) => {
+      toast.promise(createMakeup(data), {
+        loading: "Submitting form...",
+        success: () => {
+          resolve("");
+          form.reset();
+          return "Form submitted successfully";
+        },
+        error: () => {
+          resolve("");
+          return "Something went wrong";
+        },
+      });
+    });
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="max-w-[1000px] mx-auto px-5"
+      >
+        <div className="grid grid-cols-2 gap-4 mt-10 max-sm:grid-cols-1">
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <FormControl {...field}>
+                  <Input placeholder="Name..." id="name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="contact"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="contact">Contact Number</FormLabel>
+                <FormControl {...field}>
+                  <Input placeholder="Contact Number..." id="contact" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="alternateContact"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="alternateContact">
+                  Alternate Contact Number
+                </FormLabel>
+                <FormControl {...field}>
+                  <Input
+                    placeholder="Alternate Contact Number..."
+                    id="alternateContact"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="occasion"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="occasion">Occasion</FormLabel>
+                <FormControl {...field}>
+                  <Input placeholder="Occasion..." id="occasion" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="skinAllergy"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="allergies">Skin Allergies</FormLabel>
+                <FormControl {...field}>
+                  <Input
+                    placeholder="Skin Allergies (if any)..."
+                    id="allergies"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="mt-10">
+          <div className="flex justify-between">
+            <h1 className="font-bold text-3xl mb-5">Makeups</h1>{" "}
+            <Button
+              className="!p-5"
+              type="button"
+              onClick={() =>
+                append({ location: "", date: new Date(), readyTime: "" })
+              }
+            >
+              <Plus width={20} height={20} />
+            </Button>
+          </div>
+          {form.formState.errors.makeups && (
+            <p className="text-red-500">
+              {form.formState.errors.makeups.message}
+            </p>
+          )}
+          {fields.map((field, index) => (
+            <div key={field.id} className="my-5 flex flex-col gap-3.5">
+              <FormField
+                control={form.control}
+                name={`makeups.${index}.location`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="location">
+                      Location for {convertToOrdinal(index + 1)} makeup
+                    </FormLabel>
+                    <FormControl {...field}>
+                      <Input placeholder="Location..." id="location" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name={`makeups.${index}.readyTime`}
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="ready">
+                      Ready Time for {convertToOrdinal(index + 1)} makeup
+                    </FormLabel>
+                    <FormControl {...field}>
+                      <Input placeholder="Ready Time..." id="ready" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name={`makeups.${index}.date`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="date">
+                      Date for {convertToOrdinal(index + 1)} makeup
+                    </FormLabel>
+                    <FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <Button
+                            type="button"
+                            className="block"
+                            variant={"outline"}
+                          >
+                            {form.getFieldState(`makeups.${index}.date`).isDirty
+                              ? field.value.toDateString()
+                              : "Select Date"}
+                          </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="max-w-none w-fit p-0">
+                          <Calendar onSelect={field.onChange} mode="single" />
+                        </PopoverContent>
+                      </Popover>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                variant={"ghost"}
+                className="w-max"
+                type="button"
+                onClick={() => remove(index)}
+              >
+                <Trash color="red" />
+              </Button>
+              {index !== fields.length - 1 && <Separator className="mt-5" />}
+            </div>
+          ))}
+        </div>
+        <div className="my-10">
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.isSubmitting}
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default BookingForm;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ module.exports = {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
+
   theme: {
     container: {
       center: true,
@@ -16,6 +17,20 @@ module.exports = {
       },
     },
     extend: {
+      colors: {
+        primary: {
+          50: "FAE0E4",
+          100: "F7CAD0",
+          200: "F9BEC7",
+          300: "FBB1BD",
+          400: "FF99AC",
+          500: "FF85A1",
+          600: "FF7096",
+          700: "FF5C8A",
+          800: "FF477E",
+          900: "FF0A54",
+        },
+      },
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
