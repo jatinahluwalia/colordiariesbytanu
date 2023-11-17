69e6abe820528960438d2479c5dc51a36b95e6c1
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-11-17 17:29:04 +0530

    styles

--- lib/actions/makeup.action.ts
+++ lib/actions/makeup.action.ts
@@ -16,90 +16,82 @@ export const createMakeup = async (props: MakeupType) => {
       <!DOCTYPE html>
       <html lang="en">
         <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <style>
-        @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <style>
+            @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+          </style>
+          <title>Document</title>
+        </head>
+        <body
+          style="font-family: 'Roboto'; margin: 0; padding: 0; box-sizing: border-box"
+        >
+          <div style="padding-inline: min(5rem, 5vw)">
+            <h1>Hello Tanu, we have a new booking for you!</h1>
+            <p style="color: gray">Here are the details:</p>
 
-        body {
-          font-family: "Roboto";
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
+            <div
+              style="
+                display: flex;
+                flex-direction: column;
+                gap: 20px;
+                font-size: 20px;
+              "
+            >
+              <div style="display: flex; gap: 1rem; align-items: center">
+                <label for="name">Name: </label>
+                <div>${props.name}</div>
+              </div>
+              <div style="display: flex; gap: 1rem; align-items: center">
+                <label for="name">Contact: </label>
+                <div>${props.contact}</div>
+              </div>
+              <div style="display: flex; gap: 1rem; align-items: center">
+                <label for="name">Occassion: </label>
+                <div>${props.occasion}</div>
+              </div>
+              <div style="display: flex; gap: 1rem; align-items: center">
+                <label for="name">Skin Allergy: </label>
+                <div>${props.skinAllergy || "No"}</div>
+              </div>
+              <div style="display: flex; gap: 1rem; align-items: center">
+                <label for="name">Alternate Contact: </label>
+                <div>${props.alternateContact || "Not given"}</div>
+              </div>
 
-        .container {
-          padding-inline: min(5rem, 5vw);
-        }
-
-        .flex-col-container {
-          display: flex;
-          flex-direction: column;
-          gap: 20px;
-          font-size: 20px;
-        }
-
-        .detail {
-          display: flex;
-          gap: 1rem;
-          align-items: center;
-        }
-      </style>
-      <title>Document</title>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Hello Tanu, we have a new booking for you!</h1>
-      <p style="color: gray">Here are the details:</p>
-
-      <div class="flex-col-container">
-        <div class="detail">
-          <label for="name">Name: </label>
-          <div>${props.name}</div>
-        </div>
-        <div class="detail">
-          <label for="name">Contact: </label>
-          <div>${props.contact}</div>
-        </div>
-        <div class="detail">
-          <label for="name">Occassion: </label>
-          <div>${props.occasion}</div>
-        </div>
-        <div class="detail">
-          <label for="name">Skin Allergy: </label>
-          <div>${props.skinAllergy || "No"}</div>
-        </div>
-        <div class="detail">
-          <label for="name">Alternate Contact: </label>
-          <div>${props.alternateContact || "Not given"}</div>
-        </div>
-
-        <div class="makeups">
-          <h2 style="color: blue">Makeups</h2>
-          ${props.makeups.map(
-            (makeup, index) => `
-          <h5>${index + 1}.</h5>
-          <div class="flex-col-container">
-            <div class="detail">
-              <label for="name">Location: </label>
-              <div>${makeup.location}</div>
-            </div>
-            <div class="detail">
-              <label for="name">Date: </label>
-              <div>${dayjs(makeup.date).format("DD MMM, YYYY")}</div>
-            </div>
-            <div class="detail">
-              <label for="name">Ready Time: </label>
-              <div>${makeup.readyTime}</div>
+              <div class="makeups">
+                <h2 style="color: blue">Makeups</h2>
+                ${props.makeups.map(
+                  (makeup, index) => `
+                <h5>${index + 1}.</h5>
+                <div
+                  style="
+                    display: flex;
+                    flex-direction: column;
+                    gap: 20px;
+                    font-size: 20px;
+                  "
+                >
+                  <div style="display: flex; gap: 1rem; align-items: center">
+                    <label for="name">Location: </label>
+                    <div>${makeup.location}</div>
+                  </div>
+                  <div style="display: flex; gap: 1rem; align-items: center">
+                    <label for="name">Date: </label>
+                    <div>${dayjs(makeup.date).format("DD MMM, YYYY")}</div>
+                  </div>
+                  <div style="display: flex; gap: 1rem; align-items: center">
+                    <label for="name">Ready Time: </label>
+                    <div>${makeup.readyTime}</div>
+                  </div>
+                </div>
+                `
+                )}
+              </div>
             </div>
           </div>
-          `
-          )}
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
+        </body>
+      </html>
 
       `,
     });
